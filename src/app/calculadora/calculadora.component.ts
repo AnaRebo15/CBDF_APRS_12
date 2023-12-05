823bf52946ebc9c4e7078c41fd993365260ebd86
AnaRebo15 <anapaorebo15@gmail.com>
2023-12-05 14:05:14 -0600

    first commit

---
+++ src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-calculadora',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './calculadora.component.html',
+  styleUrl: './calculadora.component.css'
+})
+export class CalculadoraComponent {
+  resultado : number = 2;
+  numero1 : number = 5;
+  numero2 : number = 7;
+
+  sumarNumeros(){
+    alert("Suma de números")
+    this.resultado = this.numero1+this.numero2;
+  }
+
+  restarNumeros(){
+    alert("Resta de números")
+    this.resultado = this.numero1-this.numero2
+  }
+
+  multiplicarNumeros(){
+    alert("Multiplicación de números")
+    this.resultado = this.numero1*this.numero2
+  }
+
+  dividirNumeros(){
+    if(this.numero2 == 0)
+      alert("No se puede realizar la división")
+    else{
+      alert("División de números")
+      this.resultado = this.numero1/this.numero2
+    }
+      
+  }
+}
